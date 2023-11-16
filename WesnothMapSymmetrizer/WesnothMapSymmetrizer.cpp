971e172a6f30d66ccd76a80c02a4230c37b5a441
RedHeavenHero <redheavenhero@gmail.com>
2023-11-17 00:13:41 +0300

    better description

--- WesnothMapSymmetrizer/WesnothMapSymmetrizer.cpp
+++ WesnothMapSymmetrizer/WesnothMapSymmetrizer.cpp
@@ -14,52 +14,52 @@ import wmapsym;
 void LaunchProcess()
 {
 
-    std::string filepath_s;
-    std::cout << "\n" << "Enter Wesnoth map file path: ";
-    std::getline(std::cin, filepath_s);
-    std::filesystem::path filepath = filepath_s;
-    std::ifstream input_file{ filepath };
+	std::string filepath_s;
+	std::cout << "\n" << "Enter Wesnoth map file path: ";
+	std::getline(std::cin, filepath_s);
+	std::filesystem::path filepath = filepath_s;
+	std::ifstream input_file{ filepath };
 
-    if (!input_file.is_open())
-        throw std::exception{ "failed to open input file" };
+	if (!input_file.is_open())
+		throw std::exception{ "failed to open input file" };
 
-    std::cout << "rotation: ";
-    std::string temp;
-    std::getline(std::cin, temp);
-    int rotationDeg = temp.empty() ? 0 : std::stoi(temp);
+	std::cout << "Sample quarter rotation in degrees: ";
+	std::string temp;
+	std::getline(std::cin, temp);
+	int rotationDeg = temp.empty() ? 0 : std::stoi(temp);
 
-    WMapSym::WesnothMap map{ input_file };
-    WMapSym::Simple4PlayersSymmetrizer symmetrizer{ map, rotationDeg };
-    auto& symmetrized_map = symmetrizer.GetSymmetrizedMap();
+	WMapSym::WesnothMap map{ input_file };
+	WMapSym::Simple4PlayersSymmetrizer symmetrizer{ map, rotationDeg };
+	auto& symmetrized_map = symmetrizer.GetSymmetrizedMap();
 
-    std::filesystem::path out_filepath = filepath;
-    out_filepath.replace_filename("sym_" + filepath.filename().string());
+	std::filesystem::path out_filepath = filepath;
+	out_filepath.replace_filename("sym_" + filepath.filename().string());
 
-    std::ofstream output_file{ out_filepath };
-    if (!output_file.is_open())
-        throw std::exception{ "failed to open output file" };
+	std::ofstream output_file{ out_filepath };
+	if (!output_file.is_open())
+		throw std::exception{ "failed to open output file" };
 
-    symmetrized_map.WriteToFile(output_file);
+	symmetrized_map.WriteToFile(output_file);
 
-    std::cout << "Successfully completed" << std::endl;
-    std::cout << "Output file path: " << out_filepath.string() << std::endl;
+	std::cout << "Successfully completed" << std::endl;
+	std::cout << "Output file path: " << out_filepath.string() << std::endl;
 
 }
 
 
 void SafeLaunch()
 {
-    while (true)
-    {
-	    try
-	    {
-	    	LaunchProcess();
-	    }
-    	catch (std::exception& e)
-    	{
-    		std::cout << "ERROR: " << e.what() << std::endl;
-    	}
-    }
+	while (true)
+	{
+		try
+		{
+			LaunchProcess();
+		}
+		catch (std::exception& e)
+		{
+			std::cout << "ERROR: " << e.what() << std::endl;
+		}
+	}
 }
 
 
@@ -70,7 +70,7 @@ int main()
 
 	SafeLaunch();
 
-    return 0;
+	return 0;
 
 }
 
